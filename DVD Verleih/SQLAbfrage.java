4eeb988b2c01c2ad55dbbc638c434112092254b0
Setrexis <33389982+Setrexis@users.noreply.github.com>
2019-06-25 14:37:37 +0200

    update

--- DVD Verleih/SQLAbfrage.java
+++ DVD Verleih/SQLAbfrage.java
@@ -10,30 +10,30 @@ import java.sql.*;
  * Eine SQL-Abfrage wird an die SQLite Datenbank test.db gesendet und das
  * Ergebnis ausgegeben
  * https://www.tutorialspoint.com/sqlite/sqlite_java.htm
- * 
+ *
  */
 public class SQLAbfrage
 {
     /**
      * Wir benötigen eine Verbindung c zur SQLite Datenbankdatei, über die
-     * wir kommunizieren, d.h. Daten hin und her senden. Bei uns ist die 
-     * Datenbankdatei im selben Ordner, wie das BlueJProjekt 
+     * wir kommunizieren, d.h. Daten hin und her senden. Bei uns ist die
+     * Datenbankdatei im selben Ordner, wie das BlueJProjekt
      */
-    private Connection c; 
+    private Connection c;
     /**
      * Unsere SQL-Abfragen, SELECT * FROM COMPANY; ,ist ein String. Java muss
      * den String in eine Abfrage umwandeln, die SQLite versteht und abarbeiten
-     * kann. Hierfür gibt das Objekt stmt der Klasse Statement. 
+     * kann. Hierfür gibt das Objekt stmt der Klasse Statement.
      */
     private Statement stmt;
     /**
-     * Unsere SQL-Abfragen liefert eine Ergebnistabelle als Antwort zurück. 
+     * Unsere SQL-Abfragen liefert eine Ergebnistabelle als Antwort zurück.
      * Java muss die Ergebnistabelle in eine ResultSet umwandeln. Anschließend
      * können wir mit Java jede Zeile einzeln auslesen, bestimmte Daten herausholen
      * und danach zur nächsten Zeile springen.
      */
     private ResultSet rs;
-    
+
     private Liste list;
 
     public Liste SQLAbfrage(String abfrage) {
@@ -42,16 +42,16 @@ public class SQLAbfrage
         list = new Liste();
         try {
             /**
-             * Java sucht den aktuellen SQLiteTreiber für den Zugriff auf die 
+             * Java sucht den aktuellen SQLiteTreiber für den Zugriff auf die
              * Datenbankdatei. Bei BlueJ ist das die Datei sqlite-jdbc...
              * Die muss immer im Ordner +libs in unserem BlueJ Projekt sein.
-             * Den aktuellen Treiber gibt es kostenlos unter 
+             * Den aktuellen Treiber gibt es kostenlos unter
              * https://bitbucket.org/xerial/sqlite-jdbc/downloads/
              */
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:Filme.db");
             /**
-             * Stelle Auto Commit aus. Eine Erklärung zu SQL Transaktionen 
+             * Stelle Auto Commit aus. Eine Erklärung zu SQL Transaktionen
              * erfolgt in einigen Stunden.
              */
             c.setAutoCommit(false);
@@ -79,35 +79,35 @@ public class SQLAbfrage
 
             /**
              * Schließe die Verbindung zur SQLite Datenbankdatei.
-             */           
+             */
             c.close();
 
-        } catch ( Exception e ) 
+        } catch ( Exception e )
         {
             /**
              * Treten Fehler auf, werden die Fehler gefangen, angezeigt
-             */ 
+             */
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             /**
              * Java beendet unser Programm mit gewalt und schont den PC.
-             */ 
+             */
             System.exit(0);
         }
 
         /**
          * Falls alles ohne Fehler klappt, informiere den Benutzer auf
          * der Konsole
-         */  
+         */
         System.out.println("Operation done successfully");
         return list;
     }
 
     private void AbfrageA(String abfrage)
     {
-        try 
+        try
         {
             /**
-             * Sende die SQL-Abfrage an die Datenbank und merke die die 
+             * Sende die SQL-Abfrage an die Datenbank und merke die die
              * Ergebnistabelle mit allen Datensätzen.
              */
             rs = stmt.executeQuery(abfrage);
@@ -125,9 +125,9 @@ public class SQLAbfrage
                 int ID = rs.getInt("ID");
                 boolean Ausgeliehen = rs.getBoolean("Ausgeliehen");
                 String VerliehenAn = rs.getString("VerliehenAn");
-                
+
                 Datenelement e = new Datenelement(Erscheinungsjahr, Länge, ID, Titel, Altersbeschränkung, Genre, Ausgeliehen, VerliehenAn);
-                
+
                 list.einfügen(e);
 
                 /*
@@ -142,7 +142,7 @@ public class SQLAbfrage
                 System.out.println();
                 */
             }
-            
+
             /**
              * Gibt es keine weitere Zeile mehr in unserer Ergebnistabelle,
              * wird die SQL-Abfrage geschlossen. Danach kann man nicht mehr
@@ -150,24 +150,24 @@ public class SQLAbfrage
              * wird freigegeben.
              */
             rs.close();
-        } 
+        }
         catch ( Exception e ) {
             /**
-             * Treten Fehler auf, werden die Fehler gefangen, angezeigt und 
+             * Treten Fehler auf, werden die Fehler gefangen, angezeigt und
              * unsere Programm mit Zwang beendet.
-             */ 
+             */
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
     }
-    
-    
+
+
     private void Abfrage1Spalte(String abfrage)
     {
-        try 
+        try
         {
             /**
-             * Sende die SQL-Abfrage an die Datenbank und merke die die 
+             * Sende die SQL-Abfrage an die Datenbank und merke die die
              * Ergebnistabelle mit allen Datensätzen.
              */
             rs = stmt.executeQuery(abfrage);
@@ -178,14 +178,14 @@ public class SQLAbfrage
              */
             while ( rs.next() ) {
                 String Wert = rs.getString(1);
-                
-                
-                
 
-                
+
+
+
+
                 System.out.println( "Wert = " +Wert);
             }
-            
+
             /**
              * Gibt es keine weitere Zeile mehr in unserer Ergebnistabelle,
              * wird die SQL-Abfrage geschlossen. Danach kann man nicht mehr
@@ -193,65 +193,66 @@ public class SQLAbfrage
              * wird freigegeben.
              */
             rs.close();
-        } 
+        }
         catch ( Exception e ) {
             /**
-             * Treten Fehler auf, werden die Fehler gefangen, angezeigt und 
+             * Treten Fehler auf, werden die Fehler gefangen, angezeigt und
              * unsere Programm mit Zwang beendet.
-             */ 
+             */
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
     }
-    
-    
+
+
     public Info LoginInfo(String email){
+        SqlCreate c = new SqlCreate();
+
         int id = -1;
         byte[] pas = null;
         byte[] salt = null;
 
-        String sql = "SELECT KundenID, Passwort, salt FROM Benutzer WHERE Email='" + email + "'";
-        rs = SqlCreate.querry(sql);
-        
+        String sql = "SELECT KundenID, Passwort, salt FROM Benutzer WHERE Email='" + email + "';";
+        rs = c.querry(sql);
+
         try{
-            while(rs.next()){
-                id = rs.getInt("KundenID");
-                pas = rs.getBytes("Passwort");
-                salt = rs.getBytes("salt");
-            }
+            id = rs.getInt("KundenID");
+            pas = rs.getBytes("Passwort");
+            salt = rs.getBytes("salt");
+            c.close()
             rs.close();
         }catch(Exception e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-        
+
         Info s = new Info(id,pas,salt);
         return s;
     }
-    
+
     public int LoginAbfrage(String mail, String password){
         int id = -1;
         rs = SqlCreate.querry("SELECT KundenID FROM Benutzer WHERE Email='" + mail + "' and Passwort = '" + password + "';");
-        try 
+        try
         {
             while(rs.next()){
                 id = rs.getInt("KundenID");
             }
             rs.close();
-        } 
-        catch ( Exception e ) { 
+        }
+        catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
         return id;
     }
-    
+
     public boolean PrüfenObAusgeliehen(String Filmtitel) {
         Connection c = null;
         Statement stmt = null;
         boolean a = true;
         try{
-            
+
             String sql = "SELECT Verliehen FROM Filme WHERE Filmtitel ='" + Filmtitel + "'";
             rs = SqlCreate.querry(sql);
             a = rs.getBoolean("Verliehen");
--- DVD Verleih/SqlCreate.java
+++ DVD Verleih/SqlCreate.java
@@ -1,33 +1,37 @@
 import java.sql.*;
 /**
  * Abstrakte Klasse SqlCreate - beschreiben Sie hier die Klasse
- * 
+ *
  * @author (Ihr Name)
  * @version (eine Version-Nummer oder ein Datum)
  */
 public class SqlCreate
 {
-    public static ResultSet querry(String abfrage){
+    private Statement stmt;
+    private Connection c;
+    
+    public ResultSet querry(String abfrage){
         ResultSet rs = null;
         try{
             Class.forName("org.sqlite.JDBC");
-            Connection c = DriverManager.getConnection("jdbc:sqlite:Filme.db");
-    
+            c = DriverManager.getConnection("jdbc:sqlite:Filme.db");
+
             c.setAutoCommit(false);
-    
+
             System.out.println("Opened database successfully");
-    
-            Statement stmt = c.createStatement();
-    
+
+            stmt = c.createStatement();
+
             rs = stmt.executeQuery(abfrage);
-    
-            stmt.close();
-    
-            c.close();
         }catch(ClassNotFoundException | SQLException e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
         return rs;
+    }
+
+    public void close(){
+        stmt.close();
+        c.close();
     }
 }
